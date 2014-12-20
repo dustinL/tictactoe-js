14fb782110d03a83226aefe8a2a070fa6dcc3601
Dustin Lewis <d.lew23@gmail.com>
2014-12-20 15:15:19 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##Tic-Tac-Toe
+
+####written by Dustin Lewis
+
+==================
+
+The famous tic-tac-toe game is back in Javascript form!
---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe("Player", function() {
+  describe("initialize", function() {
+    it("is initialized with a symbol", function() {
+      var testPlayer = Object.create(Player);
+      testPlayer.initialize("X");
+      testPlayer.symbol.should.equal("X");
+    });
+  });
+});
